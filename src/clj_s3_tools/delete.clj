a3a241d7db0fd3743f777a13226a47da691083bc
Maximilian Weber <weber.maximilian@googlemail.com>
2015-03-03 15:06:34 +0100

    Added delete-dir

--- src/clj_s3_tools/delete.clj
+++ src/clj_s3_tools/delete.clj
@@ -1,5 +1,6 @@
 (ns clj-s3-tools.delete
-  (:require [amazonica.aws.s3 :as s3]))
+  (:require [amazonica.aws.s3 :as s3]
+            [clj-s3-tools.list :as l]))
 
 (defn delete-objects
   "Deletes all keys (S3 objects) in the given bucket. Returns a
@@ -15,3 +16,11 @@
          {:keys (vec batch)
           :bucket-name bucket-name}))))
     (partition-all 1000 keys))))
+
+(defn delete-dir
+  "Deletes all keys under the prefix dir-path in the given
+  bucket. Returns a sequence of deleted keys."
+  [bucket-name dir-path]
+  {:pre [(.endsWith dir-path "/")]}
+  (let [keys (map :key (l/list-object-summaries bucket-name dir-path))]
+    (delete-objects bucket-name keys)))
